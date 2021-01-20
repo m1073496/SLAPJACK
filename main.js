e42a05cf9bde6ae3839b247de80a92e9961f2ce8
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-01-19 20:24:01 -0700

    Order functions for dev empathy

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ var newGameButton = document.querySelector('.new-game-button');
 var newGame;
 
 //Event Listeners
-document.addEventListener('keydown', checkPlayerMove);
 window.addEventListener('load', startNewGame);
+document.addEventListener('keydown', checkPlayerMove);
 newGameButton.addEventListener('click', startNewGame);
 
 //Functions
@@ -24,7 +24,7 @@ function startNewGame() {
   cloak(discardPile);
   cloak(newGameButton);
   gameMsg.innerText = `SLAPJACK!`;
-};
+}
 
 function cloak(element) {
   element.classList.add('hidden');
@@ -44,43 +44,6 @@ function checkPlayerMove(e) {
   } else if (e.key === 'j') {
     newGame.slapCards(newGame.secondPlayer);
   }
-};
-
-function renderDiscard() {
-  reveal(discardPile);
-  cloak(gameMsgBox);
-  var topCardIndex = newGame.cardsInDiscardPile.length - 1;
-  var cardImage = newGame.cardsInDiscardPile[topCardIndex];
-  discardPile.innerHTML = `<img class="discard-pile" src="./assets/${cardImage}.png">`;
-}
-
-function renderMsg(player) {
-  reveal(gameMsgBox);
-  var checkBothHands = (newGame.firstPlayer.hand.length !== 0 && newGame.secondPlayer.hand.length !== 0);
-  var playerNum = findPlayerNum(player).playerNum;
-  var otherPlayerNum = findPlayerNum(player).otherPlayerNum;
-  if (newGame.cardsInDiscardPile.length >= 3 && checkBothHands && checkForSandwich()) {
-    gameMsg.innerText = `SANDWICH! PLAYER ${playerNum} TAKES THE PILE!`;
-  } else if (newGame.cardsInDiscardPile.length >= 2 && checkBothHands && checkForDoubles()) {
-    gameMsg.innerText = `DOUBLE! PLAYER ${playerNum} TAKES THE PILE!`;
-  } else if (checkForJack()) {
-    gameMsg.innerText = `SLAPJACK! PLAYER ${playerNum} TAKES THE PILE!`;
-  } else {
-    gameMsg.innerText = `BAD SLAP! PLAYER ${playerNum} FORFEITS A CARD TO PLAYER ${otherPlayerNum}!`;
-  }
-}
-
-function findPlayerNum(player) {
-  if (newGame.firstPlayer === player) {
-    playerNum = 1;
-    otherPlayerNum = 2;
-  } else if (newGame.secondPlayer === player) {
-    playerNum = 2;
-    otherPlayerNum = 1;
-  }
-  return {
-    playerNum, otherPlayerNum
-  };
 }
 
 function checkPlayerOneSlap(player) {
@@ -110,49 +73,35 @@ function checkPlayerTwoSlap(player) {
 }
 
 function checkValidity() {
-    if (newGame.cardsInDiscardPile.length >= 3) {
-      return (checkForJack() || checkForDoubles() || checkForSandwich());
-    } else if (newGame.cardsInDiscardPile.length >= 2) {
-      return (checkForJack() || checkForDoubles());
-    } else {
-      return (checkForJack());
-    }
+  if (newGame.cardsInDiscardPile.length >= 3) {
+    return (checkForJack() || checkForDoubles() || checkForSandwich());
+  } else if (newGame.cardsInDiscardPile.length >= 2) {
+    return (checkForJack() || checkForDoubles());
+  } else {
+    return (checkForJack());
   }
+}
 
 function checkForJack() {
-    var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
-    var splitString = topCard.split('-');
-    return (splitString[1] === 'jack');
-  }
+  var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
+  var splitString = topCard.split('-');
+  return (splitString[1] === 'jack');
+}
 
 function checkForDoubles() {
-    var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
-    var secondCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 2];
-    var splitFirst = topCard.split('-');
-    var splitSecond = secondCard.split('-');
-    return (splitFirst[1] === splitSecond[1]);
-  }
+  var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
+  var secondCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 2];
+  var splitFirst = topCard.split('-');
+  var splitSecond = secondCard.split('-');
+  return (splitFirst[1] === splitSecond[1]);
+}
 
 function checkForSandwich() {
-    var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
-    var thirdCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 3];
-    var splitFirst = topCard.split('-');
-    var splitThird = thirdCard.split('-');
-    return (splitFirst[1] === splitThird[1]);
-  }
-
-function renderWin(player) {
-  if (player === newGame.firstPlayer) {
-    winsPlayerOne.innerText = player.wins;
-    gameMsg.innerText = `PLAYER 1 WINS!`;
-    reveal(gameMsgBox);
-    reveal(newGameButton);
-  } else if (player === newGame.secondPlayer) {
-    winsPlayerTwo.innerText = player.wins;
-    gameMsg.innerText = `PLAYER 2 WINS!`;
-    reveal(gameMsgBox);
-    reveal(newGameButton);
-  }
+  var topCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 1];
+  var thirdCard = newGame.cardsInDiscardPile[newGame.cardsInDiscardPile.length - 3];
+  var splitFirst = topCard.split('-');
+  var splitThird = thirdCard.split('-');
+  return (splitFirst[1] === splitThird[1]);
 }
 
 function validSlap(player) {
@@ -160,12 +109,6 @@ function validSlap(player) {
   cloak(discardPile);
   reveal(stackOne);
   player.takePile(newGame, player);
-}
-
-function winGame(player) {
-  cloak(discardPile);
-  reveal(newGameButton);
-  newGame.updateWinCount(player);
 }
 
 function invalidSlap(player) {
@@ -189,10 +132,54 @@ function shuffleDiscardPile(player) {
   newGame.cardsInDiscardPile = [];
 }
 
+function winGame(player) {
+  cloak(discardPile);
+  reveal(newGameButton);
+  newGame.updateWinCount(player);
+}
+
+function renderDiscard() {
+  reveal(discardPile);
+  cloak(gameMsgBox);
+  var topCardIndex = newGame.cardsInDiscardPile.length - 1;
+  var cardImage = newGame.cardsInDiscardPile[topCardIndex];
+  discardPile.innerHTML = `<img class="discard-pile" src="./assets/${cardImage}.png">`;
+}
+
+function renderMsg(player) {
+  reveal(gameMsgBox);
+  var checkBothHands = (newGame.firstPlayer.hand.length !== 0 && newGame.secondPlayer.hand.length !== 0);
+  var playerNum = findPlayerNum(player).playerNum;
+  var otherPlayerNum = findPlayerNum(player).otherPlayerNum;
+  if (newGame.cardsInDiscardPile.length >= 3 && checkBothHands && checkForSandwich()) {
+    gameMsg.innerText = `SANDWICH! PLAYER ${playerNum} TAKES THE PILE!`;
+  } else if (newGame.cardsInDiscardPile.length >= 2 && checkBothHands && checkForDoubles()) {
+    gameMsg.innerText = `DOUBLE! PLAYER ${playerNum} TAKES THE PILE!`;
+  } else if (checkForJack()) {
+    gameMsg.innerText = `SLAPJACK! PLAYER ${playerNum} TAKES THE PILE!`;
+  } else {
+    gameMsg.innerText = `BAD SLAP! PLAYER ${playerNum} FORFEITS A CARD TO PLAYER ${otherPlayerNum}!`;
+  }
+}
+
 function renderPlayerDeck(player) {
   if (findPlayerNum(player).playerNum === 1) {
     reveal(stackOne);
   } else if (findPlayerNum(player).playerNum === 2) {
     reveal(stackTwo);
+  }
+}
+
+function renderWin(player) {
+  if (player === newGame.firstPlayer) {
+    winsPlayerOne.innerText = player.wins;
+    gameMsg.innerText = `PLAYER 1 WINS!`;
+    reveal(gameMsgBox);
+    reveal(newGameButton);
+  } else if (player === newGame.secondPlayer) {
+    winsPlayerTwo.innerText = player.wins;
+    gameMsg.innerText = `PLAYER 2 WINS!`;
+    reveal(gameMsgBox);
+    reveal(newGameButton);
   }
 }
